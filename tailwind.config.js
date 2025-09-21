330b915ba58a7d98368a94a3fc8a251c1edc5f81
MB DEV <mbdev.web13@gmail.com>
2025-09-21 16:53:11 +0200

    import tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        pfBlueLight: "#9AC8EB",
+        pfBlue: "#5784BA",
+        pfPink: "#F4CFDF",
+        pfBlueSoft: "#B6D8F2",
+        pfYellow: "#F7F6CF",
+      },
+      fontFamily: { sans: ["Inter", "system-ui", "sans-serif"] },
+    },
+  },
+  plugins: [],
+}
